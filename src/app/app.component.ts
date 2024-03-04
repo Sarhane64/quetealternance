33e22ae8b04724e525659983409db7715b198171
jean519 <129189905+jean519@users.noreply.github.com>
2024-03-04 14:56:02 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, Router } from '@angular/router';
-import { FooterComponent } from './footer/footer.component.js';
-import { HeaderComponent } from './header/header.component.js';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { HomeComponent } from './home/home.component.js';
 import { UserProfileComponent } from './user-profile/user-profile.component.js';
 import { SignUpComponent } from './sign-up/sign-up.component.js';
@@ -11,8 +9,6 @@ import { SignUpComponent } from './sign-up/sign-up.component.js';
   standalone: true,
   imports: [
     RouterOutlet,
-    FooterComponent,
-    HeaderComponent,
     HomeComponent,
     RouterLink,
     UserProfileComponent,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { FooterComponent } from './footer/footer.component.js';
-import { HeaderComponent } from './header/header.component.js';
 import { HomeComponent } from './home/home.component.js';
+import { SignUpComponent } from './sign-up/sign-up.component.js';
+import { UserProfileComponent } from './user-profile/user-profile.component.js';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'signup', component: HeaderComponent },
-  { path: 'user', component: FooterComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: 'footer', component: UserProfileComponent },
   { path: '**', redirectTo: '/home', pathMatch: 'full' },
 ];
